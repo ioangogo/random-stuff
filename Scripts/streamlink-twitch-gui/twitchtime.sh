262f7863ae30defd36c80288fe1626f902f1c9ca
Ioan Loosley <legit.ioangogo@gmail.com>
2020-06-21 11:58:22 +0100

    Twitch-Time: made some changes

--- Scripts/streamlink-twitch-gui/twitchtime.sh
+++ Scripts/streamlink-twitch-gui/twitchtime.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
 H=$(date "+%k")
-if (( [ 21 -le $H ] && [ $H -lt 23 ] ) || ( [ 0 -le $H ] && [ $H -lt 6 ] )); then 
+MONITORS=$(xrandr -q | grep " connected" | wc -l)
+#MONITORS=1
+if ( [ 21 -le $H ] && [ $H -lt 23 ]  ||  [ 0 -le $H ] && [ $H -lt 6 ] ) && [ $MONITORS -gt 1 ]
+then 
     MPV_PROFILE="twitchbed"
 else 
     MPV_PROFILE="twitchpip"
 fi
 
+echo "$MPV_PROFILE"
 exec mpv --profile="$MPV_PROFILE" "$@"
